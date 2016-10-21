6a06edb274b4f325bf3f1db7fb6c5b0bac60fa5e
Nathan Morin <nathanamorin@gmail.com>
2016-10-21 08:43:34 -0400

    Added Presence & changed to automate URL creation from Google Sheet Key

--- getDoorData.py
+++ getDoorData.py
@@ -1,17 +1,19 @@
 import urllib2
 import csv
 
+GoogleSheetsURL = 'https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key={key}&exportFormat=csv'
 
 #Dictionary of google sheets export urls
-DataUrls = {
-	'Wireless_Door_Lab':'https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=147Gf1f3z-r1weIikSwSSd741baTqqqRkc06716gF09c&exportFormat=csv'
+DocumentKeys = {
+	'Wireless_Door_Lab':'147Gf1f3z-r1weIikSwSSd741baTqqqRkc06716gF09c',
+	'Presence':'1MAg21Z2H01UHSK1WiAzxfcqziD-CYvMnpafM_XPXwMs'
 }
 
 #Retrieves data from dict of urls & places CSV data into corresponding dict
 def getCSVData(urls):
 	csvData = {}
 	for urlName in urls.keys():
-		csvData[urlName] = csv.reader(urllib2.urlopen(urls[urlName]))
+		csvData[urlName] = csv.reader(urllib2.urlopen(GoogleSheetsURL.replace('{key}',urls[urlName])))
 
 	return csvData
 
@@ -19,7 +21,8 @@ def getCSVData(urls):
 
 
 #Example usage, downloads and prints data for the Wireless Door Lab
-for event in getCSVData(DataUrls)['Wireless_Door_Lab']:
+CSVData = getCSVData(DocumentKeys)
+for event in CSVData['Wireless_Door_Lab']:
 	for field in event:
 		print field + '\t'
 
